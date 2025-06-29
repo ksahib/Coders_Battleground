eb1ceb719429ccdc9d09d9caf236597bc352f043
ksahib <kazi.sahib@northsouth.edu>
2025-06-30 02:19:04 +0600

    navigation added

---
+++ root/assets/js/login_signup.js
@@ -0,0 +1,13 @@
+
+
+const loginSection = document.querySelector('.login-section');
+const registerLink = document.querySelector('.register-link');
+const loginLink = document.querySelector('.login-link');
+
+registerLink.addEventListener('click', () => {
+  loginSection.classList.add('active');
+});
+
+loginLink.addEventListener('click', () => {
+  loginSection.classList.remove('active');
+});
