4e26a8817241056cb9ff5de9f38233ce4283465f
ksahib <kazi.sahib@northsouth.edu>
2025-06-30 15:45:09 +0600

    navigation finalised

--- root/assets/js/weeklycontest.js
+++ root/assets/js/weeklycontest.js
@@ -1,11 +1,9 @@
 function showTab(tabId) {
-  // if tabId is 'ranking', redirect instead of toggling
   if (tabId === 'ranking') {
     window.location.href = '../pages/live-standings.html';
     return; 
   }
 
-  // otherwise do your normal in‑page tab switch:
   var tabs = document.querySelectorAll('.tab-content');
   tabs.forEach(tab => tab.classList.remove('active'));
   document.getElementById(tabId).classList.add('active');
